9fa61a56e524d37956cd13238a2c38ec5da6c1cf
YagmurKaranfil <59535304+YagmurKaranfil@users.noreply.github.com>
2022-11-02 13:20:14 +0300

    week4_1

---
+++ Kodlama.io.Devs/src/main/java/kodlama/io/Kodlama/io/Devs/business/abstracts/LanguageService.java
@@ -0,0 +1,13 @@
+package kodlama.io.Kodlama.io.Devs.business.abstracts;
+
+import java.util.List;
+
+import kodlama.io.Kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
+public interface LanguageService { 
+	List<ProgrammingLanguage> getAll();
+	ProgrammingLanguage getById(int id);
+	void add(ProgrammingLanguage programmingLanguage) throws Exception;
+	void delete(int id);
+	void update(ProgrammingLanguage programmingLanguage);
+
+}
---
+++ Kodlama.io.Devs/src/main/java/kodlama/io/Kodlama/io/Devs/webApi/LanguagesController.java
@@ -0,0 +1,58 @@
+package kodlama.io.Kodlama.io.Devs.webApi;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlama.io.Kodlama.io.Devs.business.abstracts.LanguageService;
+import kodlama.io.Kodlama.io.Devs.entities.concretes.ProgrammingLanguage;
+
+@RestController
+@RequestMapping("/api/ProgrammingLanguages")
+public class LanguagesController {
+	private LanguageService languageServise;
+
+	@Autowired
+	public LanguagesController(LanguageService languageServise) {
+		super();
+		this.languageServise = languageServise;
+	}
+	
+	@GetMapping("/getAll")
+	public List<ProgrammingLanguage>getAll() {
+		return languageServise.getAll();
+	}
+
+	
+	@GetMapping("/getById")
+	public ProgrammingLanguage getById(int id) {
+		return languageServise.getById(id);
+	
+     }
+	
+	@PostMapping("/add")
+	public void add(ProgrammingLanguage programmingLanguage) throws Exception {
+		languageServise.add(programmingLanguage);
+	}
+	
+	@DeleteMapping("/delete")
+	public void delete( int id) {
+		languageServise.delete(id);
+	}
+	
+	@PutMapping("/update")
+	public void update(ProgrammingLanguage programmingLanguage) {
+		languageServise.update(programmingLanguage);
+	}
+	
+	
+	
+	
+	
+}
